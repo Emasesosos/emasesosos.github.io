121defdc497f0130accb47cef3e822305f4bd39e
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2019-07-28 19:51:31 -0500

    Aplicación 21-CRUD/REACTJS/REDUX/FIREBASE/FIRESTORE

---
+++ precache-manifest.0f21a7eaac0342bace05c3ea152fcb5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f351ecc1c9ed44b6085574f85d517b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e28d948b658413b8bae8",
+    "url": "/static/css/main.013dd1b0.chunk.css"
+  },
+  {
+    "revision": "4019b061bb5f366a87d4",
+    "url": "/static/js/2.7722065e.chunk.js"
+  },
+  {
+    "revision": "e28d948b658413b8bae8",
+    "url": "/static/js/main.5403fb13.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "497ef353c11d1b487836eb18490b2b49",
+    "url": "/static/media/fbLogo.497ef353.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "8db5010752a9c3fe4f5092387dcf73ae",
+    "url": "/static/media/reduxLogo.8db50107.svg"
+  }
+]);
